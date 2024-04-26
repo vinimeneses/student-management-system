777fe176c05c0a4cbf457637387da46883940c86
Vinícius Meneses <viniciusmenesesdev@gmail.com>
2024-04-26 09:36:36 -0300

    first commit

---
+++ src/main/java/com/javaguides/sms/entity/Student.java
@@ -0,0 +1,24 @@
+package com.javaguides.sms.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+}
