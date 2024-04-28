09f7e37dba3b570d0d0954cbbef7a4fb65878d9c
Vinícius Meneses <viniciusmenesesdev@gmail.com>
2024-04-28 11:35:25 -0300

    implement edit student feature

--- src/main/java/com/javaguides/sms/controller/StudentController.java
+++ src/main/java/com/javaguides/sms/controller/StudentController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -42,12 +43,37 @@ public class StudentController {
     @PostMapping("/students")
     public String saveStudent(@Valid @ModelAttribute("student") StudentDto student,
                               BindingResult result,
-                              Model model){
+                              Model model) {
         if (result.hasErrors()) {
             model.addAttribute("student", student);
             return "create_student";
         }
         studentService.createStudent(student);
+        return "redirect:/students";
+    }
+
+    // handler method to handle edit student request
+    @GetMapping("/students/{studentId}/edit")
+    public String editStudent(@PathVariable("studentId") Long studentId,
+                              Model model) {
+        StudentDto student = studentService.getStudentById(studentId);
+        model.addAttribute("student", student);
+        return "edit_student";
+    }
+
+    // handler method to handle edit student form submit request
+
+    @PostMapping("/students/{studentId}")
+    public String updateStudent(@PathVariable("studentId") Long studentId,
+                                @Valid @ModelAttribute("student") StudentDto studentDto,
+                                BindingResult result,
+                                Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("student", studentDto);
+            return "edit_student";
+        }
+        studentDto.setId(studentId);
+        studentService.updateStudent(studentId, studentDto);
         return "redirect:/students";
     }
 }
--- src/main/java/com/javaguides/sms/service/impl/StudentServiceImpl.java
+++ src/main/java/com/javaguides/sms/service/impl/StudentServiceImpl.java
@@ -34,4 +34,16 @@ public class StudentServiceImpl implements StudentService {
         studentRepository.save(student);
     }
 
+    @Override
+    public StudentDto getStudentById(Long studentId) {
+        Student student = studentRepository.findById(studentId).get();
+        StudentDto studentDto = StudentMapper.mapToStudentDto(student);
+        return studentDto;
+    }
+
+    @Override
+    public void updateStudent(Long studentId, StudentDto student) {
+        studentRepository.save(StudentMapper.mapToStudent(student));
+    }
+
 }
